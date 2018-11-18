7e2acf58228d427dde3347b14171e8499bd08169
althea <altheaserena.yi@gmail.com>
2018-11-17 20:30:51 -0500

    Creates list url, create url

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# bungaloo
+# Bungaloo
+Bungaloo is home rental API. Use it to create and manage a listing of homes for rent.
+
+## What can it do?
+1. Create a home listing
+1. Retrieve a home listing
+1. Retrieve a list of all home listings
+1. Update a home listing
+1. Delete a home listing
--- bungaloo/api/views.py
+++ bungaloo/api/views.py
@@ -4,12 +4,17 @@ from . models import Home
 
 class CreateView(generics.ListCreateAPIView):
     """This class defines the create behaviour of our api"""
-    queryset = Home.objects.all()
+    queryset = Home.objects.all()[:0]
     serializer_class = HomeSerializer
 
     def perform_create(self, serializer):
         """Save the post data when creating a new home"""
         serializer.save()
+
+class ListView(generics.ListAPIView):
+    """This class lists all the homes"""
+    queryset = Home.objects.all()
+    serializer_class = HomeSerializer
 
 class DetailsView(generics.RetrieveUpdateDestroyAPIView):
     """This class handles GET, PUT and DELETE requests"""
